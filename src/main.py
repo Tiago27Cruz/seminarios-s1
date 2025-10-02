176aaf71d8dbefa36905275cab05cc0482a25744
Tiago Cruz <tiagocruz057@gmail.com>
2025-10-02 23:21:30 +0100

    Added commands to facilitate running the program

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import wikidata as wd
+import argparse
 
 STEAMDB = "../resources/steam.csv"
 STEAMLINKED = "../resources/steam_wikidata.csv"
 
 def readCsvData(filePath: str) -> pd.DataFrame:
-    df = pd.read_csv(filePath)
-    return df
+    try:
+        df = pd.read_csv(filePath)
+        return df
+    except Exception as e:
+        print(f"Error reading {filePath}: {e}")
+        return pd.DataFrame()
 
 def removeGames(df: pd.DataFrame) -> pd.DataFrame:
     def filter_by_owners(row):
@@ -25,12 +30,31 @@ def saveCsvData(df: pd.DataFrame, filePath: str) -> None:
     df.to_csv(filePath, index=False)
     print(f"Data saved to {filePath}")
 
+def createParser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Process Steam data.")
+    parser.add_argument('--output', type=str, default=STEAMLINKED, help='Output CSV file path (with linked data and data processing)')
+    parser.add_argument('-c', '--create', action='store_true', help='Create a new CSV file with linked data')
+    parser.add_argument('--input', type=str, default=STEAMDB, help='Input CSV file path')
+    return parser
+
 def main():
-    df = readCsvData(STEAMDB)
-    df = removeGames(df)
-    df = wd.getGameWDEntry(df)
-    df.head()
-    saveCsvData(df, "../resources/steam_wikidata.csv")
+    parser = createParser()
+    args = parser.parse_args()
+    
+    if args.create:
+        print("Creating new CSV with linked data @ " + args.output)
+        df = readCsvData(args.input)
+        df = removeGames(df)
+        df = wd.getGameWDEntry(df)
+        saveCsvData(df, args.output)
+    else:
+        if not args.input or args.input == STEAMDB:
+            args.input = STEAMLINKED
+        print("Reading existing CSV with linked data @ " + args.input)
+        df = readCsvData(args.input)
+
+    print(df.head())
+    
 
 if __name__ == "__main__":
     main()
